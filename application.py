6dbaeb70e543f777df6124a456bbcfc6fd6f1eed
Joris Arends <jorisarends@me.com>
2020-01-15 11:27:00 +0000

    Database bewerkt

--- application.py
+++ application.py
@@ -69,6 +69,6 @@ def game_over():
 
 @app.route("/quiz", methods=["GET", "POST"])
 def quiz():
-    Q = question(1, 16, 'easy', get_token()) # Test voor API function
+    Q = question(1, 18, 'easy', get_token()) # Test voor API function
     return render_template("quiz.html", Q = Q)
 
