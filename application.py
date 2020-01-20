e254ac0f7b04b5d71386cb656e4357e6d8efa258
Shilpa Kedar <shilpa@live.nl>
2020-01-20 11:53:41 +0000

    quiz

--- application.py
+++ application.py
@@ -66,7 +66,6 @@ def leaderboards():
 def about():
     return render_template("about.html")
 
-
 @app.route("/game_over", methods=["GET", "POST"])
 def game_over():
     return render_template("game_over.html")
@@ -76,5 +75,3 @@ def game_over():
 def quiz():
     Q = question(1, 18, 'easy', get_token()) # Test voor API function
     return render_template("quiz.html", Q = Q)
-
-
--- static/game_over.js
+++ static/game_over.js
@@ -1,42 +1,14 @@
-// const username = document.getElementById("username");
 const saveScoreBtn = document.getElementById("saveScoreBtn");
 const finalScore = document.getElementById("finalScore");
 const mostRecentScore = localStorage.getItem("mostRecentScore");
 
-// geen highscore return lege array
-const highScores = JSON.parse(localStorage.getItem("highScores")) || [];
-// console.log(highScores);
-// const MAX_HIGH_SCORES = 5;
-
 finalScore.innerText = mostRecentScore;
 
-// username.addEventListener("keyup", () => {
-//     // console.log(username.value);
-
-//     // if no username is given than can not click button
-//     saveScoreBtn.disabled = !username.value;
-// });
-
 saveHighScore = e => {
+
+  // if no username is given than can not click button
   console.log("clicked the save button!");
   e.preventDefault();
-// };
 
-  // const score = {
-  //   // score = highScores
-  //   score: Math.floor(Math.random() * 100),
-  //   // name: username.value
-  // };
-  // highScores.push(score);
-
-  // // sorteer highscores van hoog naar laag
-  // highScores.sort((a, b) => b.score - a.score);
-  // highScores.splice(5);
-
-
-  // localStorage.setItem("highScores", JSON.stringify(highScores));
-
-  // als score is saved go back naar index
-  // window.location.assign("/");
-  console.log(highScores);
+  window.location.assign("/");
 };
