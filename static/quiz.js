JorisArends/Trivia-WebIK06

commit 18a8e80a372f12f53182f6b3430181d8030c5049

Timer gemaakt die de totale tijd bijhoud, laat zien en in de database doet

static/quiz.js
@@ -108,8 +108,8 @@ choices.forEach(choice => {
 
 		else if (classToApply === "incorrect") {
 			localStorage.setItem("mostRecentScore", score);
-			$.get('/insert_score',{username: username, score: score, category: category});
-		// 	return window.location.assign("/game_over");
+			$.get('/insert_score',{username: username, score: score, category: category, time: time});
+		 	return window.location.assign("/game_over");
 		}
 		if ($(e.target).hasClass('choice-container')) {
 		  selectedChoice.classList.add(classToApply);
@@ -145,3 +145,24 @@ var decodeHTML = function (html) {
 	txt.innerHTML = html;
 	return txt.value;
 };
+
+
+
+let count = 0;
+let intervalRef = null;
+
+intervalRef = setInterval(_ => {
+  count+=10;
+
+  let s = Math.floor((count /  1000)) % 60;
+  let m = Math.floor((count / 60000)) % 60;
+  if(m<10){
+  	m = "0"+ m;
+  }
+  if(s<10){
+  	s = "0" + s;
+  }
+	time = m + ":" + s
+  $('#timer').text(time);
+}, 10);
+

templates/quiz.html
@@ -7,28 +7,33 @@
 {% block main %}
 <div class= "container_quiz">
 	<div id= "game" class= "justify-center flex-column">
-<p>{{ username | tojson }} - {{ category | tojson }} - {{ token | tojson}}</p>
 		<div id= "punten">
 			<div id= "punten-item">
 				<p class= "punten-prefix"> <strong> Score </strong></p>
 				<h1 class= "punten-main-text" id= "score">0</h1>
 			</div>
+			<div id="punten-item" style="width:100px">
+				<p class= "punten-prefix"><strong>Tijd</strong></p>
+				<h1 class= "punten-main-text" id="timer">00:00</h1>
+
+			</div>
+
 		</div>
 
 		<h2 id= "question"></h2>
-		<div class= "choice-container" data-number= "1">
+		<div class= "choice-container">
 		<p class= "choice-prefix"> A </p>
 		<p class= "choice-text" data-number= "1" id="1"></p>
 		</div>
-		<div class= "choice-container" data-number= "2" >
+		<div class= "choice-container">
 		<p class= "choice-prefix"> B </p>
 		<p class= "choice-text" data-number= "2" id="2"></p>
 		</div>
-		<div class= "choice-container" data-number= "3">
+		<div class= "choice-container">
 		<p class= "choice-prefix"> C </p>
 		<p class= "choice-text" data-number= "3" id="3"></p>
 		</div>
-		<div class= "choice-container" data-number= "4">
+		<div class= "choice-container">
 		<p class= "choice-prefix"> D </p>
 		<p class= "choice-text" data-number= "4" id="4"></p>
 		</div>

application.py
@@ -101,11 +101,12 @@ def insert_score():
     username = request.args.get('username')
     score = int(request.args.get('score'))
     category = request.args.get('category')
+    time = request.args.get('time')
     prev_score = db.execute("SELECT * FROM scores WHERE naam = ? AND categorie = ?", (username, category))
     if score > 0:
         if not prev_score:
-            db.execute("INSERT INTO scores (naam, vragen, tijd, categorie) VALUES(?, ?, ?, ?);", (username, score, "00:00", category))
+            db.execute("INSERT INTO scores (naam, vragen, tijd, categorie) VALUES(?, ?, ?, ?);", (username, score, time, category))
         else:
             if score > prev_score[0]["vragen"]:
-                db.execute("UPDATE scores SET vragen = ?, tijd = ? WHERE naam = ? AND categorie = ?", (score, "00:00", username, category))
+                db.execute("UPDATE scores SET vragen = ?, tijd = ? WHERE naam = ? AND categorie = ?", (score, time, username, category))
     return('', 204)

templates/about.html
@@ -12,26 +12,30 @@
     <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur eu finibus neque. Sed vel ex porta, auctor nibh id, lobortis ex. Maecenas finibus sollicitudin tincidunt. Proin eget augue enim. Fusce in libero fermentum, tincidunt eros nec, imperdiet elit. Suspendisse in lorem pharetra, ullamcorper libero dignissim, fermentum urna. Proin lacinia quis elit quis vehicula. Praesent ullamcorper mi vitae maximus porttitor. Nullam id aliquam leo. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Mauris condimentum id magna eu dapibus.</p>
     </div>
     <div class="row">
-                    <div class="col">
-                        <img src="/static/images/Joris.png" alt="Joris" style="width:100%">
-                        <div>
-                            <h4><b>Joris</b></h4>
-                            <a>Integer auctor non turpis vel accumsan. Mauris viverra sapien fermentum enim placerat, et porttitor augue convallis. Maecenas a dolor egestas neque pulvinar cursus quis a ex. Duis laoreet sagittis lectus, porttitor rhoncus ex imperdiet imperdiet. Aenean lobortis, dui a dignissim finibus, quam nisl ultrices augue, id molestie metus ex eget velit. Etiam lectus arcu, dapibus vitae ullamcorper eu, varius nec massa. Praesent consectetur interdum tortor vitae lacinia. Proin sagittis vel elit sit amet lacinia. Nullam bibendum egestas accumsan. Aenean odio sapien, hendrerit a ipsum vel, volutpat dictum lacus. In rhoncus vestibulum blandit.</a>
-                        </div>
-                    </div>
-					<div class="col">
-                        <img src="/static/images/Shilpa.png" alt="Shilpa" style="width:100%">
-                        <div>
-                            <h4><b>Shilpa</b></h4>
-                            <p>Donec lobortis neque felis, vel dapibus nisi dictum dictum. Sed molestie interdum purus, maximus malesuada mi malesuada nec. Sed et augue id est volutpat mattis. Ut rutrum nisi quis dolor venenatis, vel dapibus ex porta. Curabitur suscipit id velit non pulvinar. Curabitur ac malesuada erat, pellentesque pretium nibh. Proin vel massa nec ante pharetra ultrices eget sed sem. Ut aliquet lorem vel dui maximus malesuada. Nullam magna lectus, varius eget imperdiet eget, faucibus vitae lorem. Nulla viverra a arcu sed porttitor. Aenean pharetra congue aliquet.</p>
-                        </div>
-                    </div>
-					<div class="col">
-                        <img src="/static/images/Maarten.png" alt="Maarten" style="width:100%">
-                        <div>
-                            <h4><b>Maarten</b></h4>
-                            <p>Nullam commodo est leo, at maximus quam semper vel. Mauris varius tempus tortor, a pharetra diam varius vel. Duis eget lorem placerat, sodales lorem nec, tincidunt neque. Nunc varius justo in lectus lobortis, et suscipit augue auctor. Nam commodo ipsum ac viverra ultricies. Sed porta tortor vitae eros varius, at dictum felis tincidunt. Proin pharetra lorem ipsum, eget pulvinar ex sodales vel. Suspendisse purus orci, tristique tincidunt efficitur sit amet, posuere at quam. Pellentesque in posuere orci. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia Curae; Aliquam erat volutpat.</p>
-                        </div>
-                    </div>
-                </div>
+        <div class="col">
+            <img src="/static/images/Joris.png" alt="Joris" style="width:100%">
+            <div>
+                <h4><b>Joris</b></h4>
+                <a>Integer auctor non turpis vel accumsan. Mauris viverra sapien fermentum enim placerat, et porttitor augue convallis. Maecenas a dolor egestas neque pulvinar cursus quis a ex. Duis laoreet sagittis lectus, porttitor rhoncus ex imperdiet imperdiet. Aenean lobortis, dui a dignissim finibus, quam nisl ultrices augue, id molestie metus ex eget velit. Etiam lectus arcu, dapibus vitae ullamcorper eu, varius nec massa. Praesent consectetur interdum tortor vitae lacinia. Proin sagittis vel elit sit amet lacinia. Nullam bibendum egestas accumsan. Aenean odio sapien, hendrerit a ipsum vel, volutpat dictum lacus. In rhoncus vestibulum blandit.</a>
+            </div>
+        </div>
+		<div class="col">
+            <img src="/static/images/Shilpa.png" alt="Shilpa" style="width:100%">
+            <div>
+                <h4><b>Shilpa</b></h4>
+                <p>Donec lobortis neque felis, vel dapibus nisi dictum dictum. Sed molestie interdum purus, maximus malesuada mi malesuada nec. Sed et augue id est volutpat mattis. Ut rutrum nisi quis dolor venenatis, vel dapibus ex porta. Curabitur suscipit id velit non pulvinar. Curabitur ac malesuada erat, pellentesque pretium nibh. Proin vel massa nec ante pharetra ultrices eget sed sem. Ut aliquet lorem vel dui maximus malesuada. Nullam magna lectus, varius eget imperdiet eget, faucibus vitae lorem. Nulla viverra a arcu sed porttitor. Aenean pharetra congue aliquet.</p>
+            </div>
+        </div>
+		<div class="col">
+            <img src="/static/images/Maarten.png" alt="Maarten" style="width:100%">
+            <div>
+                <h4><b>Maarten</b></h4>
+                <p>Nullam commodo est leo, at maximus quam semper vel. Mauris varius tempus tortor, a pharetra diam varius vel. Duis eget lorem placerat, sodales lorem nec, tincidunt neque. Nunc varius justo in lectus lobortis, et suscipit augue auctor. Nam commodo ipsum ac viverra ultricies. Sed porta tortor vitae eros varius, at dictum felis tincidunt. Proin pharetra lorem ipsum, eget pulvinar ex sodales vel. Suspendisse purus orci, tristique tincidunt efficitur sit amet, posuere at quam. Pellentesque in posuere orci. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia Curae; Aliquam erat volutpat.</p>
+            </div>
+        </div>
+    </div>
+
+
+
+
 {% endblock %}

templates/leaderboards.html
@@ -11,7 +11,7 @@
     <form action="/leaderboards" method="post" id="form">
         <center>
         <div class="input-group">
-            <select required id="inputGroupSelect04", name = "categorie">
+            <select id="inputGroupSelect04", name = "categorie">
                 <option value=0>Select category</option>
                 <option value=9>General Knowledge</option>
                 <option value=17>Science & Nature</option>
@@ -54,25 +54,24 @@
 <br>
 <br>
 
-// <script>
-// document.getElementById("form").addEventListener("submit", validate);
+<script>
+ document.getElementById("form").addEventListener("submit", validate);
 
-//         function validate(e) {
+         function validate(e) {
 
-//             //get username from form
-//             var option = document.getElementById("inputGroupSelect04").value;
+             //get username from form
+             var option = document.getElementById("inputGroupSelect04").value;
+             //stop form from being submitted
+             e.preventDefault();
 
-//             //stop form from being submitted
-//             e.preventDefault();
+             if(option === "0") {
+                 alert("You haven't selected anything!");
+             }
+             else{
+                 document.getElementById("form").submit();
+             }
 
-//             if(option === "0") {
-//                 alert("You haven't selected anything!");
-//             }
-//             else{
-//                 document.getElementById("form").submit();
-//             }
+         }
 
-//         }
-
-// </script>
+</script>
 {% endblock %}

commit a4361f43cc56dd9ee25da6ee36eddca97f585db4

fix something broken in 18a8e80

static/quiz.js
@@ -87,15 +87,14 @@ getNewQuestion = () =>  {
 };
 
 choices.forEach(choice => {
-    choice.parentElement.addEventListener("click", e => {
-    	console.log(e.target);
-        if(!acceptingAnswers) return;
+  choice.parentElement.addEventListener("click", e => {
+    console.log(e.target);
+    if(!acceptingAnswers) return;
 
-        acceptingAnswers = false;
+    acceptingAnswers = false;
 
-        const selectedChoice = e.target;
-        const selectedAnswer = selectedChoice.dataset["number"];
-        console.log(selectedChoice);
+    const selectedChoice = e.target;
+    const selectedAnswer = selectedChoice.dataset["number"];
 
 		// antwoord correct/incorrect
 		//console.log(selectedAnswer == currentQuestion.answer);
@@ -149,7 +148,7 @@ var decodeHTML = function (html) {
 };
 
 
-  // Countdown timer per question of 10 seconds
+// Countdown timer per question of 10 seconds
 function countdown() {
   var timeleft = 10;
   var counter = 0;

templates/quiz.html
@@ -28,19 +28,19 @@
 		</div>
 
 		<h2 id= "question"></h2>
-		<div class= "choice-container">
+		<div class= "choice-container" data-number= "1">
 		<p class= "choice-prefix"> A </p>
 		<p class= "choice-text" data-number= "1" id="1"></p>
 		</div>
-		<div class= "choice-container">
+		<div class= "choice-container" data-number= "2">
 		<p class= "choice-prefix"> B </p>
 		<p class= "choice-text" data-number= "2" id="2"></p>
 		</div>
-		<div class= "choice-container">
+		<div class= "choice-container" data-number= "3">
 		<p class= "choice-prefix"> C </p>
 		<p class= "choice-text" data-number= "3" id="3"></p>
 		</div>
-		<div class= "choice-container">
+		<div class= "choice-container" data-number= "4">
 		<p class= "choice-prefix"> D </p>
 		<p class= "choice-text" data-number= "4" id="4"></p>
 		</div>